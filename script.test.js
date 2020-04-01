ed22299ec149172ba69d4360094a203283cec733
IbrahimTarabay <ibrahimesalah69@gmail.com>
2020-04-01 16:49:07 +0200

    add more tests

--- script.test.js
+++ script.test.js
@@ -7,14 +7,26 @@ dbMock = [
   'dogpictures.com'
 ];
 
-it('is a silly test',() => {
-  expect('hello').toBe('hello')
-})
-
-it('is searching google', () => {
-  expect(googleSearch('testtest',dbMock))
+describe('googleSearch', () => {
+  it('is a silly test',() => {
+    expect('hello').toBe('hello')
+  })
+  
+  it('is searching google', () => {
+    expect(googleSearch('testtest',dbMock))
+       .toEqual([])
+    expect(googleSearch('dog',dbMock))
+       .toEqual(['dog.com','dogpictures.com'])   
+  })
+  
+  it('work with undefined and null input', () => {
+    expect(googleSearch(undefined,dbMock))
      .toEqual([])
-  expect(googleSearch('dog',dbMock))
-     .toEqual(['dog.com','dogpictures.com'])   
+     expect(googleSearch(null,dbMock))
+     .toEqual([]) 
+  })
+  
+  it('does not return more than 3 matches', () => {
+    expect(googleSearch('.com',dbMock).length).toEqual(3);
+  })
 })
-
